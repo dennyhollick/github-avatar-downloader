449e45238a9b0f8f9e8837dc27280e82aff161e7
Denny <dennyhollick@gmail.com>
2017-07-04 04:01:27 +0000

    added download image function. Will download and save images.

--- download_avatars.js
+++ download_avatars.js
@@ -1,6 +1,7 @@
 var request = require('request');
+var fs = require('fs');
 
-//fill in your access token here instead of process.argv[2];
+//fill in your github personal access token here instead of process.argv[2];, create one here: https://github.com/settings/tokens
 
 var accessToken = process.argv[2];
 
@@ -24,6 +25,7 @@ function getRequestOptions(repoOwner, repoName) {
 // Gets the API data
 
 function getRepoContributors(repoOwner, repoName, callback) {
+  console.log("Looking for and downloading images from " + repoName + " to /avatar... Please wait.");
   request(getRequestOptions(repoOwner, repoName), function (error, response, body) {
     try {
       const data = JSON.parse(body);
@@ -34,12 +36,27 @@ function getRepoContributors(repoOwner, repoName, callback) {
   });
 }
 
+// downloads all of the image URLs after they have been fetched to specified filepath.
+
+function downloadImageByURL(url, filePath) {
+  request.get(url)
+    .on('error', function (err) {
+    throw err;
+  })
+    .on('response', function (response){
+      response.pipe(fs.createWriteStream(filePath));
+    })
+    .on('end', function (){
+      console.log("Downloading: " + url + "   ========>   " + filePath);
+    });
+  }
+
 
 // Calls the function with the inputs of the username, repo, and callback function that filters out only avatar URLs
 
 getRepoContributors("jquery", "jquery", function (data) {
   data.forEach(function(data) {
-    console.log(data.avatar_url);
+    downloadImageByURL(data.avatar_url, "avatars/" + data.login + ".jpg");
   });
 });
 
